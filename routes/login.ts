22e614a48dfaa3543b2a8d1ef6710b79dc213055
Cousky <bengana.yassine63@gmail.com>
2024-02-08 18:35:28 +0100

    fix: SQLi

--- routes/login.ts
+++ routes/login.ts
@@ -35,7 +35,11 @@ module.exports = function login () {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
      
     // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    models.sequelize.query(`SELECT * FROM Users WHERE email = ? AND password = ? AND deletedAt IS NULL`, 
+    { model: UserModel, 
+      plain: true,
+      replacements: [req.body.email || '',
+      security.hash(req.body.password || '')] })
     .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
