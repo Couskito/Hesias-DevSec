8b86dd28fbab13a075da22e8e0e2f7b77994a363
Cousky <bengana.yassine63@gmail.com>
2024-02-08 18:35:04 +0100

    clean: removed ci/cd

--- routes/login.ts
+++ routes/login.ts
@@ -33,7 +33,6 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    console.log("coucou");
      
     // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
     models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
